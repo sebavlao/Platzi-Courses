80fafbfe95275a936aaf6c872fb7ec48cd4d1a96
Sebavlao-ubuntu <sebav364@gmail.com>
2021-12-14 17:29:21 -0300

    Terminado el curso de dependencias de arrays en javascript

--- intermedio/Curso de Ecmascript 6/src/es11/index.js
+++ intermedio/Curso de Ecmascript 6/src/es11/index.js
@@ -1,6 +1,50 @@
+// Clase 12
+
 const button = document.getElementById("btn");
 
 button.addEventListener("click", async function() {
     const module = await import("./file.js");
     module.hello();
 });
+
+
+// Clase 13
+
+const aBigNumber = 9007199254740991n;
+
+const anotherBigNumber = BigInt(9007199254740991);
+
+console.log(aBigNumber);
+console.log(anotherBigNumber);
+
+//
+
+const promise1 = new Promise((resolve, reject) => reject("reject"));
+const promise2 = new Promise((resolve, reject) => reject("resolve"));
+const promise3 = new Promise((resolve, reject) => reject("resolve 1"));
+
+Promise.allSettled([promise1, promise2, promise3])
+    .then(response => console.log(response));
+
+//
+
+console.log(window);
+console.log(globalThis);
+
+//
+
+        /* "asd" */
+const fooo = null ?? "default string";
+console.log(fooo);
+
+//
+
+const user = {};
+
+console.log(user?.profile?.email);
+
+if (user?.profile?.email) {
+    console.log(`email`);
+} else {
+    console.log(`fail`);
+}
